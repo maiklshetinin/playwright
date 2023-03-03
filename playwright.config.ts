7ece6a3feab73436e08154d0f2129ec91c0cdecc
shchetinin <shchetinin@stdpr.ru>
2023-03-03 18:17:04 +0300

    refactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   workers: 4,
   testMatch: [
-    //--------------------стабильны при 4х воркерах
     "tests/1.test.ts",
     "tests/2.test.ts",
     "tests/3.test.ts",
@@ -13,23 +12,13 @@ const config: PlaywrightTestConfig = {
     "tests/7.test.ts",
     "tests/8.test.ts",
     "tests/9.test.ts",
-
-    "tests/10.test.ts",
-
+    // "tests/10_16_17.test.ts",
     "tests/11.test.ts",
     "tests/12.test.ts",
     "tests/13.test.ts",
     "tests/14.test.ts",
-    // //--------------------стабильны при 3х воркерах
     "tests/15.test.ts",
-    //--------------------стабильны при 4х воркерах
-
-    "tests/16.test.ts",
-    // "tests/17.test.ts",
-
-    //--------------------стабильны при 3х воркерах
     "tests/18.test.ts",
-    //--------------------стабильны при 4х воркерах
     "tests/19.test.ts",
     "tests/20.test.ts",
     "tests/21.test.ts",
